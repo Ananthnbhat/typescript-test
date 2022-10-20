17ae2664f4ccbb53789c540cb73c2aa90a992556
Ananthnbhat <ananthnbhat2@gmail.com>
2022-10-21 08:44:45 +1100

    generate tsconfig for TS and add jest,gen jest config

---
+++ index.ts
@@ -0,0 +1,23 @@
+// module.exports = dotpath;
+type ObjectWithPath = {
+  [key: string]: any;
+};
+
+export default function dotpath(str: string) {
+  const parts:string[] = str.split(".");
+  const len:number = parts.length;
+
+  return function parse(obj: ObjectWithPath) {
+    var testKey;
+
+    for (var i = 0; i < len; ++i) {
+      testKey = parts[i];
+
+      if (!obj) return;
+
+      obj = obj[testKey];
+    }
+
+    return obj;
+  };
+}
